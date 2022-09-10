4a642c00df549f4833e8238642fcbcd85d82ea9a
Elisa <acrazypie5@gmail.com>
2022-09-10 14:40:51 +0200

    First Commit

---
+++ src/main/java/net/thesadpie/randomthings/RandomThings.java
@@ -0,0 +1,15 @@
+package net.thesadpie.randomthings;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RandomThings implements ModInitializer {
+	public static final String MOD_ID = "randomthings";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thesadpie/randomthings/RandomThingsClient.java
@@ -0,0 +1,10 @@
+package net.thesadpie.randomthings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
